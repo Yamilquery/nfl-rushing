e311355ca4ec0c3f6bbde5b818184a89b82542b0
Yamil Díaz Aguirre <yamilquery@gmail.com>
2020-03-01 09:59:58 -0600

    create football player model

---
+++ lib/nfl_rushing/football_player/models/football_player.ex
@@ -0,0 +1,38 @@
+defmodule NflRushing.FootballPlayers.Model.FootballPlayer do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "football_players" do
+    field :player_name, :string
+    field :team_abbreviation, :string
+    field :player_position, :string
+    field :rushing_attemps_per_game_avg, :float
+    field :rushing_attemps, :float
+    field :total_rushing_yards, :float
+    field :rushing_average_yards_per_attempt, :float
+    field :rushing_yards_per_game, :float
+    field :total_rushing_touchdowns, :float
+    field :longest_rush, :float
+    field :rushing_first_downs, :float
+    field :rushing_first_down_percentage, :float
+    field :rushing_20_yards_each, :float
+    field :rushing_40_yards_each, :float
+    field :rushing_fumbles, :float
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(%__MODULE__{} = post, attrs) do
+    post
+    |> cast(attrs, [:player_name, :team_abbreviation, :player_position,
+      :rushing_attemps_per_game_avg, :rushing_attemps, :total_rushing_yards,
+      :rushing_average_yards_per_attempt, :rushing_yards_per_game,
+      :total_rushing_touchdowns, :longest_rush, :rushing_first_downs,
+      :rushing_first_down_percentage, :rushing_20_yards_each,
+      :rushing_40_yards_each, :rushing_fumbles
+    ])
+    |> validate_required([:player_name, :rushing_attemps_per_game_avg])
+  end
+
+end
